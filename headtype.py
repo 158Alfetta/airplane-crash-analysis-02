3cc457b1eb46afaf62c3e7c48c76be977e65d746
Udomeak <60070120@kmitl.ac.th>
2017-11-07 18:15:10 +0700

    Debug error on counting
    just make 2 dict to recheck the same type and add it again in final loop this will improve about the same name like 'douglas DC-3' and 'douglas DC-3C' obiously it is same types but differance engine(minor)

--- headtype.py
+++ headtype.py
@@ -4,15 +4,20 @@ def headtype():
     """plane Type"""
     count = 0
     types = {}
-    head = pd.read_csv('C:\\Users\\DELL\\Desktop\\dataset_psit.csv')
+    type_all = {}
+    head = pd.read_csv('C:\\Users\\Test\\Documents\\GitHub\\airplane-crash-analysis-02\\dataset_psit.csv', encoding='ISO-8859-1')
     head_type = head.Type.tolist()
     result_type = dict([(i, head_type.count(i)) for i in set(head_type)])
     typesorted = sorted(result_type.items(), key=lambda x: x[1], reverse=True)
-    for key, value in typesorted:
-        if count == 10:
-            break
+    for key, value in typesorted:   #make new list types means top 10
+        if count > 10:
+            type_all[key] = value
         else:
             types[key] = value
             count += 1
+    for key_2 in types:     #recheck about the same type in other key in dict() type_all
+        for key_3 in type_all:
+            if key_2 in str(key_3):
+                types[key_2] += type_all[key_3]
     return types
 headtype()
