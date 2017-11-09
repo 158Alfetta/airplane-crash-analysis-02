a1008371ecfc4ce5dbe02aec7baa26191c941865
Punmanat <60070053@kmitl.ac.th>
2017-11-09 18:37:44 +0700

    Fixing descrition

--- chart_years.py
+++ chart_years.py
@@ -8,10 +8,10 @@ def main():
 def year():
 	dict_ = years.allyear()
 	line_chart = pygal.HorizontalBar()
-	line_chart.title = 'Amount of fatalities in plane crash (years)'
+	line_chart.title = 'Numbers of plane crash per years from 1970 - 2016'
 	keys = [key for key in dict_.keys()]
 	keys.sort(reverse=True)
-	line_chart.add('Fatalities', [dict_[key] for key in keys])
+	line_chart.add('Per years', [dict_[key] for key in keys])
 	line_chart.x_labels = keys
 	line_chart.render_to_file('line_chart.svg')
 
