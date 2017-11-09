8d8972e7d2884ebaae90f1daa943067969e6b360
Udomeak <60070120@kmitl.ac.th>
2017-11-09 21:15:18 +0700

    change some NaN object

--- F-ground.py
+++ F-ground.py
@@ -1,24 +1,25 @@
 import pandas as pd
 def ground():
-    df = pd.read_csv('C:\\Users\\Test\\Desktop\\airplane-crash-analysis\\clean_data_year.csv', encoding = "ISO-8859-1")
-    date = df['Date']
+    df = pd.read_csv('C:\\Users\\Test\\Documents\\GitHub\\airplane-crash-analysis-02\\dataset_psit.csv', encoding = "ISO-8859-1")
+    date = df.Date.tolist()
     years = []
     diction = {}
     diction_sort = {}
-    count = 0
     for item in date:
         temp = item.split()
         years.append(temp[2])
     ground = df.Ground.tolist()
+    flight = df.Flight.fillna('unknown').tolist()
     summary = df.Summary.tolist()
-    flight = df.Flight.tolist()
+    for j in range(len(flight)):
+        if flight[j] == '?':
+            flight[j] = 'Unknown'
     for i in range(len(ground)):
         if int(ground[i]) != 0:
             diction[int(ground[i])] = [years[i], flight[i], summary[i]]
     for j in sorted(diction, reverse=True):
         diction_sort[j] = diction[j]
-        if count == 10:
-            break
-return diction_sort # just only top ten ground people
-#return diction  # all data that ground != 0
+    print(diction_sort)
+    return diction_sort # just only top ten ground people
+    #return diction  # all data that ground != 0
 ground()
