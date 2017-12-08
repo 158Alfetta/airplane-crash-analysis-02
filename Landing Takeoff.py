ce46fd04b74b9b851340e2c9ad6bd49dce000a43
Yonlada Prasert <60070077@kmitl.ac.th>
2017-12-08 18:00:04 +0700

    yonandizecalculator

---
+++ Landing Takeoff.py
@@ -0,0 +1,26 @@
+stalled = 15
+engine = 59
+weather = 41
+improper = 35
+maintance = 16
+lack = 25
+inadequate = 18
+error = 21
+overran = 18
+fog = 36
+overload = 4
+weight = 3
+
+
+t_stalled = 52
+t_engine = 230 
+t_weather = 43
+t_improper = 38
+t_maintance = 28
+t_lack = 2
+t_inadequate = 28
+t_error = 17
+t_overran = 25
+t_fog = 20
+t_overload =35
+t_weight = 16
